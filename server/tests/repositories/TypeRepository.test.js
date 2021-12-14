a430324daf43954fc3094d0e5f0d760da7f22c22
Andrew Richerson <ajorigami1@gmail.com>
2021-12-14 01:13:40 -0600

    Add some TypeRepository.getAll tests

--- server/tests/repositories/TypeRepository.test.js
+++ server/tests/repositories/TypeRepository.test.js
@@ -1,5 +1,7 @@
+import ActivityType from "../../src/model/ActivityType.js";
 import { TypeRepository } from "../../src/repositories/TypeRepository.js";
 import { dbConnect, dbDisconnect } from "../dbHandler.utils.js";
+import { fakeActivityType } from "../fixtures/index.js";
 
 beforeAll(async () => await dbConnect());
 afterAll(async () => await dbDisconnect());
@@ -12,5 +14,19 @@ describe("getAll", () => {
         expect(actual).toEqual(expected);
     });
 
-    test("should return array when some types", () => {});
+    test("should return array when some types", async () => {
+        const type = new ActivityType(fakeActivityType);
+        await type.save();
+        expect(type).toBeTruthy();
+
+        const typeRepo = new TypeRepository();
+
+        const result = await typeRepo.getAll();
+
+        // TODO: make expectActivityTypeArrayEqual and expectActivityTypesEqual
+        expect(result).toHaveLength(1);
+
+        const returnedType = result[0];
+        expect(returnedType.name).toEqual(fakeActivityType.name);
+    });
 });
