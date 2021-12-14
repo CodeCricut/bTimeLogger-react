e02bd0e9a380836cddc89fe6b72c7334f749a59f
Andrew Richerson <ajorigami1@gmail.com>
2021-12-14 01:20:14 -0600

    Update TypeRepository tests to use expect helpers

--- server/tests/repositories/TypeRepository.test.js
+++ server/tests/repositories/TypeRepository.test.js
@@ -2,6 +2,7 @@ import ActivityType from "../../src/model/ActivityType.js";
 import { TypeRepository } from "../../src/repositories/TypeRepository.js";
 import { dbConnect, dbDisconnect } from "../dbHandler.utils.js";
 import { fakeActivityType } from "../fixtures/index.js";
+import { expectActivityTypeArraysEqual } from "../util/expect-helpers.js";
 
 beforeAll(async () => await dbConnect());
 afterAll(async () => await dbDisconnect());
@@ -11,7 +12,7 @@ describe("getAll", () => {
         const typeRepo = new TypeRepository();
         const expected = [];
         const actual = await typeRepo.getAll();
-        expect(actual).toEqual(expected);
+        expectActivityTypeArraysEqual(expected, actual);
     });
 
     test("should return array when some types", async () => {
@@ -21,12 +22,10 @@ describe("getAll", () => {
 
         const typeRepo = new TypeRepository();
 
-        const result = await typeRepo.getAll();
+        const expected = [type];
+        const actual = await typeRepo.getAll();
 
-        // TODO: make expectActivityTypeArrayEqual and expectActivityTypesEqual
-        expect(result).toHaveLength(1);
-
-        const returnedType = result[0];
-        expect(returnedType.name).toEqual(fakeActivityType.name);
+        expect(actual).toHaveLength(1);
+        expectActivityTypeArraysEqual(expected, actual);
     });
 });
