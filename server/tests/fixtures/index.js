c403a781ae1f8487a5141101dfdfd22f191b92d9
Andrew Richerson <ajorigami1@gmail.com>
2021-12-14 02:16:09 -0600

    Add unit tests for getById

--- server/tests/fixtures/index.js
+++ server/tests/fixtures/index.js
@@ -1,3 +1,5 @@
 export const fakeActivityType = {
     name: "FAKE_ACTIVITY_TYPE_NAME",
 };
+
+export const NON_EXISTANT_ID = "abc783a04b5e80aba5258999";
--- server/tests/repositories/TypeRepository.test.js
+++ server/tests/repositories/TypeRepository.test.js
@@ -1,8 +1,14 @@
 import ActivityType from "../../src/model/ActivityType.js";
+import IdNotProvidedError from "../../src/repositories/errors/IdNotProvidedError.js";
+import InvalidIdFormatError from "../../src/repositories/errors/InvalidIdFormatError.js";
+import NotFoundError from "../../src/repositories/errors/NotFoundError.js";
 import { TypeRepository } from "../../src/repositories/TypeRepository.js";
 import { dbConnect, dbDisconnect, resetDb } from "../dbHandler.utils.js";
-import { fakeActivityType } from "../fixtures/index.js";
-import { expectActivityTypeArraysEqual } from "../util/expect-helpers.js";
+import { fakeActivityType, NON_EXISTANT_ID } from "../fixtures/index.js";
+import {
+    expectActivityTypeArraysEqual,
+    expectActivityTypesEqual,
+} from "../util/expect-helpers.js";
 
 beforeAll(async () => {
     dbConnect(); // awaiting will let afterEach run; must run to completion
@@ -14,6 +20,13 @@ afterAll(async () => {
 
 afterEach(async () => resetDb());
 
+const addFakeActivityType = async () => {
+    const type = new ActivityType(fakeActivityType);
+    await type.save();
+    expect(type).toBeTruthy();
+    return type;
+};
+
 describe("getAll", () => {
     test("should return empty array when no types", async () => {
         const typeRepo = new TypeRepository();
@@ -23,9 +36,7 @@ describe("getAll", () => {
     });
 
     test("should return array when some types", async () => {
-        const type = new ActivityType(fakeActivityType);
-        await type.save();
-        expect(type).toBeTruthy();
+        const type = await addFakeActivityType();
 
         const typeRepo = new TypeRepository();
 
@@ -44,11 +55,42 @@ describe("getAll", () => {
         const expected = [];
 
         // Add a type
-        const type = new ActivityType(fakeActivityType);
-        await type.save();
-        expect(type).toBeTruthy();
+        await addFakeActivityType();
 
         expect(actual).toHaveLength(0);
         expectActivityTypeArraysEqual(expected, actual);
+    });
+});
+
+describe("getById", () => {
+    test("should return one with valid id", async () => {
+        const expected = await addFakeActivityType();
+        const typeRepo = new TypeRepository();
+
+        const actual = await typeRepo.getById(expected.id);
+
+        expectActivityTypesEqual(expected, actual);
+    });
+
+    test("should throw if id not provided", async () => {
+        const typeRepo = new TypeRepository();
+        expect(async () => {
+            await typeRepo.getById(null);
+        }).rejects.toThrow(IdNotProvidedError);
+    });
+
+    test("should throw if invalid id provided", async () => {
+        const typeRepo = new TypeRepository();
+
+        expect(async () => {
+            await typeRepo.getById("INVALID ID");
+        }).rejects.toThrow(InvalidIdFormatError);
+    });
+
+    test("should throw if not found", async () => {
+        const typeRepo = new TypeRepository();
+        expect(async () => {
+            await typeRepo.getById(NON_EXISTANT_ID);
+        }).rejects.toThrow(NotFoundError);
     });
 });
