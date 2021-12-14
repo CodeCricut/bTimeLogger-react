c1a3daad4b080bec39aecc824ba9b85304ae31d7
Andrew Richerson <ajorigami1@gmail.com>
2021-12-14 01:56:57 -0600

    Properly reset test environment in TypeRepository tests

--- server/tests/repositories/TypeRepository.test.js
+++ server/tests/repositories/TypeRepository.test.js
@@ -1,11 +1,18 @@
 import ActivityType from "../../src/model/ActivityType.js";
 import { TypeRepository } from "../../src/repositories/TypeRepository.js";
-import { dbConnect, dbDisconnect } from "../dbHandler.utils.js";
+import { dbConnect, dbDisconnect, resetDb } from "../dbHandler.utils.js";
 import { fakeActivityType } from "../fixtures/index.js";
 import { expectActivityTypeArraysEqual } from "../util/expect-helpers.js";
 
-beforeAll(async () => await dbConnect());
-afterAll(async () => await dbDisconnect());
+beforeAll(async () => {
+    dbConnect(); // awaiting will let afterEach run; must run to completion
+});
+
+afterAll(async () => {
+    dbDisconnect();
+});
+
+afterEach(async () => resetDb());
 
 describe("getAll", () => {
     test("should return empty array when no types", async () => {
@@ -26,6 +33,22 @@ describe("getAll", () => {
         const actual = await typeRepo.getAll();
 
         expect(actual).toHaveLength(1);
+        expectActivityTypeArraysEqual(expected, actual);
+    });
+
+    test("should not return types added after call", async () => {
+        const typeRepo = new TypeRepository();
+
+        // Get all
+        const actual = await typeRepo.getAll();
+        const expected = [];
+
+        // Add a type
+        const type = new ActivityType(fakeActivityType);
+        await type.save();
+        expect(type).toBeTruthy();
+
+        expect(actual).toHaveLength(0);
         expectActivityTypeArraysEqual(expected, actual);
     });
 });
