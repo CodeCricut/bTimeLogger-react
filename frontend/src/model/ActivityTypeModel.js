010d1571cbfcd37456bfc41e8cab45e38e7b54cd
Andrew Richerson <ajorigami1@gmail.com>
2021-12-15 16:46:22 -0600

    Implement ActivityTypeRepository

---
+++ frontend/src/model/ActivityTypeModel.js
@@ -0,0 +1,46 @@
+/**
+ * Represents a single activity type.
+ */
+class ActivityTypeModel {
+    /**
+     * The string representation of the type's id.
+     * @type {string}
+     */
+    _id;
+
+    /**
+     * The name of the type.
+     * @type {string}
+     */
+    name;
+
+    constructor(id, name) {
+        this._id = id;
+        this.name = name;
+    }
+
+    /**
+     * Simple accessor alternative to the `_id` property.
+     * @type {string}
+     */
+    get id() {
+        return this._id;
+    }
+}
+
+const mapObjectToModel = (obj) => {
+    if (!obj._id) throw new Error("Object missing _id field.");
+    if (!obj.name) throw new Error("Object missing name field.");
+
+    return new ActivityTypeModel(obj._id, obj.name);
+};
+
+const mapObjectsToModels = (objects) => {
+    const models = [];
+    for (const obj in objects) {
+        models.push(mapObjectToModel(obj));
+    }
+    return models;
+};
+
+export { ActivityTypeModel, mapObjectToModel, mapObjectsToModels };
--- frontend/src/repositories/ActivityTypeRepository.js
+++ frontend/src/repositories/ActivityTypeRepository.js
@@ -1,4 +1,9 @@
 import axios from "axios";
+import {
+    ActivityTypeModel,
+    mapObjectToModel,
+    mapObjectsToModels,
+} from "../model/ActivityTypeModel.js";
 
 /**
  * Class for interacting with the ActivityType model using the REST API.
@@ -6,35 +11,52 @@ import axios from "axios";
 class ActivityTypeRepository {
     /**
      * Get an array of all activity types
-     * @returns {Promise<Array<ActivityTypeApiModel>>}
-     * @throws TODO
+     * @returns {Promise<Array<ActivityTypeModel>>}
+     * @throws {Error} Will throw if API does not indicate success.
      */
-    async getAll() {}
+    async getAll() {
+        const response = await axios.get("/types");
+        if (response.status !== 200) throw new Error(response.error);
+        return mapObjectsToModels(response.data);
+    }
 
     /**
      * Get an activity type by its id
      * @param {string} id The id of the type
-     * @returns {Promise<ActivityTypeApiModel>}
-     * @throws TODO
+     * @returns {Promise<ActivityTypeModel>}
+     * @throws {Error} Will throw if API does not indicate success.
      */
-    async getById(id) {}
+    async getById(id) {
+        if (!id) throw new Error("Tried to get activity type without id.");
+        const response = await axios.get(`types/${id}`);
+        if (response.status !== 200) throw new Error(response.error);
+        return mapObjectToModel(response.data);
+    }
 
     /**
      * Add a new type and return it.
-     * @param {ActivityTypeApiModel} type The activity type to add
+     * @param {ActivityTypeModel} type The activity type to add
      * @param {string} type.name The name of the type to add
-     * @returns {Promise<ActivityTypeApiModel>} The added activity
-     * @throws TODO
+     * @returns {Promise<ActivityTypeModel>} The added activity
+     * @throws {Error} Will throw if API does not indicate success.
      */
-    async add(type) {}
+    async add(type) {
+        const response = await axios.post(`types/add`, type);
+        if (response.status !== 200) throw new Error(response.error);
+        return mapObjectToModel(response.data);
+    }
 
     /**
      * Delete a type from the database.
      * @param {string} id The id of the type to remove
      * @returns {Promise}
-     * @throws TODO
+     * @throws {Error} Will throw if API does not indicate success.
      */
-    async remove(id) {}
+    async remove(id) {
+        const response = await axios.delete(`types/remove/${id}`);
+        if (response.status !== 200) throw new Error(response.error);
+        return mapObjectToModel(response.data);
+    }
 }
 
 export { ActivityTypeRepository };
