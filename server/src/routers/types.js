a085b31a3eeafd95f93bf00660a091b57f93ed02
Andrew Richerson <ajorigami1@gmail.com>
2021-12-14 18:29:51 -0600

    Add error handling middleware to types router

--- server/src/routers/types.js
+++ server/src/routers/types.js
@@ -4,96 +4,62 @@ import IdNotProvidedError from "../repositories/errors/IdNotProvidedError.js";
 import InvalidIdFormatError from "../repositories/errors/InvalidIdFormatError.js";
 import MissingModelInfoError from "../repositories/errors/MissingModelInfoError.js";
 import NotFoundError from "../repositories/errors/NotFoundError.js";
+import InvalidDateError from "../repositories/errors/InvalidDateError.js";
+
 import { TypeRepository } from "../repositories/TypeRepository.js";
 
 const router = express.Router();
 const typeRepo = new TypeRepository();
 
-router.get("/", async (req, res) => {
-    try {
-        const types = await typeRepo.getAll();
-        res.json(types);
-    } catch (e) {
+router.get("/", async (req, res, next) => {
+    const types = await typeRepo.getAll().catch(next);
+
+    res.json(types);
+});
+
+router.get("/:id", async (req, res, next) => {
+    const type = await typeRepo.getById(req.params.id).catch(next);
+
+    res.status(200);
+    res.json(type);
+});
+
+router.post("/add", async (req, res, next) => {
+    const type = await typeRepo.add(req.body.name).catch(next);
+
+    res.status(200);
+    res.json(type);
+});
+
+router.delete("/remove/:id", async (req, res, next) => {
+    await typeRepo.delete(req.params.id).catch(next);
+
+    res.status(200);
+    res.send();
+});
+
+router.use((e, req, res, next) => {
+    if (
+        e instanceof AlreadyAddedError ||
+        e instanceof IdNotProvidedError ||
+        e instanceof InvalidDateError ||
+        e instanceof InvalidIdFormatError ||
+        e instanceof MissingModelInfoError
+    ) {
+        // Invalid req
+        res.status(400);
+        res.send(e.message);
+    } else if (e instanceof NotFoundError) {
+        // Not found
+        res.status(404);
+        res.send(e.message);
+    } else {
+        // Server error
         console.error(e);
         res.status(500);
-        res.send();
-    }
-});
-
-router.get("/:id", async (req, res) => {
-    try {
-        const type = await typeRepo.getById(req.params.id);
-
-        res.status(200);
-        res.json(type);
-    } catch (e) {
-        if (
-            e instanceof IdNotProvidedError ||
-            e instanceof InvalidIdFormatError
-        ) {
-            // Invalid req
-            res.status(400);
-            res.send(e.message);
-        } else if (e instanceof NotFoundError) {
-            // Not found
-            res.status(404);
-            res.send(e.message);
-        } else {
-            // Server error
-            console.error(e);
-            res.status(500);
-            res.send("Internal server error.");
-        }
-    }
-});
-
-router.post("/add", async (req, res) => {
-    try {
-        const type = await typeRepo.add(req.body.name);
-
-        res.status(200);
-        res.json(type);
-    } catch (e) {
-        if (
-            e instanceof MissingModelInfoError ||
-            e instanceof AlreadyAddedError
-        ) {
-            // Invalid req
-            res.status(400);
-            res.send(e.message);
-        } else {
-            // Server error
-            console.error(e);
-            res.status(500);
-            res.send("Internal server error.");
-        }
-    }
-});
-
-router.delete("/remove/:id", async (req, res) => {
-    try {
-        await typeRepo.delete(req.params.id);
-
-        res.status(200);
-        res.send();
-    } catch (e) {
-        if (
-            e instanceof MissingModelInfoError ||
-            e instanceof InvalidIdFormatError
-        ) {
-            // Invalid req
-            res.status(400);
-            res.send(e.message);
-        } else if (e instanceof NotFoundError) {
-            // Not found
-            res.status(404);
-            res.send(e.message);
-        } else {
-            // Server error
-            console.error(e);
-            res.status(400);
-            res.send("Internal server error.");
-        }
+        res.send(
+            "There was an internal server error while handling the request."
+        );
     }
 });
 
