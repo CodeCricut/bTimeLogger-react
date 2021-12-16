a2b2aba949ca08aff7fa2f2eb1f367ba3b37847c
Andrew Richerson <ajorigami1@gmail.com>
2021-12-16 04:22:47 -0600

    Test helpers for ActivityModel tests

---
+++ frontend/src/test-helpers/fixtures/activities.js
@@ -0,0 +1,18 @@
+export const allActivities = [
+    {
+        _id: "_id1",
+        type: "type1",
+        comment: "comment1",
+        startTime: "2021-12-13T04:20:00.361Z",
+        endTime: "2021-12-13T04:40:00.361Z",
+        trashed: false,
+    },
+    {
+        _id: "_id2",
+        type: "type2",
+        comment: "comment2",
+        startTime: "2021-12-13T04:30:00.361Z",
+        endTime: "2021-12-13T04:40:00.361Z",
+        trashed: true,
+    },
+];
--- frontend/src/test-helpers/util/expect-helpers.js
+++ frontend/src/test-helpers/util/expect-helpers.js
@@ -11,18 +11,19 @@ export const expectActivityTypeArraysEqual = (expected, actual) => {
     }
 };
 
-// export const expectActivitiesEqual = (expected, actual) => {
-//     expect(actual.type).toEqual(expected.type);
-//     expect(actual.startTime).toEqual(expected.startTime);
-//     expect(actual.endTime).toEqual(expected.endTime);
-//     expect(actual.comment).toEqual(expected.comment);
-//     expect(actual.trashed).toBe(expected.trashed);
-// };
+export const expectActivitiesEqual = (expected, actual) => {
+    expect(actual._id).toEqual(expected._id);
+    expect(actual.type).toEqual(expected.type);
+    expect(actual.comment).toEqual(expected.comment);
+    expect(actual.startTime).toEqual(expected.startTime);
+    expect(actual.endTime).toEqual(expected.endTime);
+    expect(actual.trashed).toBe(expected.trashed);
+};
 
-// export const expectActivitiesArrayEqual = (expected, actual) => {
-//     if (expected.length !== actual.length)
-//         fail("Activity arrays were of different lengths.");
-//     for (let i = 0; i < expected.length; i++) {
-//         expectActivitiesEqual(expected[i], actual[i]);
-//     }
-// };
+export const expectActivitiesArrayEqual = (expected, actual) => {
+    if (expected.length !== actual.length)
+        fail("Activity arrays were of different lengths.");
+    for (let i = 0; i < expected.length; i++) {
+        expectActivitiesEqual(expected[i], actual[i]);
+    }
+};
