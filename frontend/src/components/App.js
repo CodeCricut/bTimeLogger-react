869aa53afb81c32e70c6d83aec6b75a933611f6d
Andrew Richerson <ajorigami1@gmail.com>
2021-12-17 21:28:43 -0600

    Remove ActivityContext and ActivityTypeContext as per #37

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { CssBaseline } from "@material-ui/core";
 import Moment from "react-moment";
 
-import { ActivityTypeProvider } from "../activity-types/ActivityTypeContext.js";
-import { ActivityProvider } from "../activities/ActivityContext";
 import { ThemeSwitcherProvider } from "../style/ThemeSwitcherContext";
 
 import Layout from "./Layout";
@@ -13,14 +11,10 @@ Moment.globalFormat = "MM/DD HH:mm";
 
 function App() {
     return (
-        <ActivityProvider>
-            <ActivityTypeProvider>
-                <ThemeSwitcherProvider>
-                    <CssBaseline />
-                    <Layout />
-                </ThemeSwitcherProvider>
-            </ActivityTypeProvider>
-        </ActivityProvider>
+        <ThemeSwitcherProvider>
+            <CssBaseline />
+            <Layout />
+        </ThemeSwitcherProvider>
     );
 }
 
