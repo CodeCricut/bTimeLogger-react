32b8977b424c827a21c96ba77ba0e5621708ec4c
Andrew Richerson <ajorigami1@gmail.com>
2021-12-20 11:23:26 -0600

    Add theme and theme switcher

--- frontend/src/style/ThemeSwitcherContext.js
+++ frontend/src/style/ThemeSwitcherContext.js
@@ -5,17 +5,23 @@ import { DARK, darkTheme, lightTheme } from "./theme.js";
 
 const ThemeSwitcherContext = createContext();
 
+/**
+ * Global theme provider.
+ */
 const ThemeSwitcherProvider = ({ children }) => {
     const [theme, setTheme] = useState(DARK);
     const appTheme = theme === DARK ? { ...darkTheme } : { ...lightTheme };
 
     return (
-        <ThemeSwitcherContext.Provider value={[theme, setTheme]}>
+        <ThemeSwitcherContext.Provider value={setTheme}>
             <ThemeProvider theme={appTheme}>{children}</ThemeProvider>
         </ThemeSwitcherContext.Provider>
     );
 };
 
+/**
+ * @returns {setTheme}
+ */
 const useThemeSwitcherContext = () => {
     return useContext(ThemeSwitcherContext);
 };
