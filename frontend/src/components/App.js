d3f3adde47aead505f6ffed7de62a6e81ebff7d1
Andrew Richerson <ajorigami1@gmail.com>
2021-12-21 15:33:35 -0600

    Use composition for Layout

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,12 +3,84 @@ import { CssBaseline } from "@mui/material";
 import { ThemeSwitcherProvider } from "../style/ThemeSwitcherContext";
 
 import Layout from "./Layout";
+import ActivityList from "./ActivityList.js";
+import Activity from "./Activity.js";
+import CompletedActivityMenu from "./CompletedActivityMenu";
+import Duration from "./Duration.js";
+import useDate from "../hooks/useDate.js";
+
+import { useActivityRepository } from "../activities/useActivityRepository.js";
+import StartedActivityMenu from "./StartedActivityMenu.js";
 
 function App() {
+    const [activityState, {}] = useActivityRepository();
+    const currentDate = useDate();
+
+    const handleEdit = () => {
+        console.log("handle edit");
+    };
+
+    const handleResume = () => {
+        console.log("handle resume");
+    };
+
+    const handleTrash = () => {
+        console.log("handle trash");
+    };
+
+    const handleStop = () => {
+        console.log("handle stop");
+    };
+
     return (
         <ThemeSwitcherProvider>
             <CssBaseline />
-            <Layout />
+            <Layout
+                renderStartActivity={() => {}}
+                renderActivityList={() => (
+                    <ActivityList
+                        activities={activityState.activities}
+                        renderCompletedActivity={(activity) => (
+                            <Activity
+                                activity={activity}
+                                renderMenu={() => (
+                                    <CompletedActivityMenu
+                                        handleEdit={() => handleEdit(activity)}
+                                        handleResume={() =>
+                                            handleResume(activity)
+                                        }
+                                        handleTrash={() =>
+                                            handleTrash(activity)
+                                        }
+                                    />
+                                )}
+                                renderDuration={() => (
+                                    <Duration
+                                        startDate={activity.startTimeDate}
+                                        endDate={activity.endTimeDate}
+                                    />
+                                )}
+                            />
+                        )}
+                        renderStartedActivity={(activity) => (
+                            <Activity
+                                activity={activity}
+                                renderMenu={() => (
+                                    <StartedActivityMenu
+                                        handleStop={handleStop}
+                                    />
+                                )}
+                                renderDuration={() => (
+                                    <Duration
+                                        startDate={activity.startTimeDate}
+                                        endDate={currentDate}
+                                    />
+                                )}
+                            />
+                        )}
+                    />
+                )}
+            />
         </ThemeSwitcherProvider>
     );
 }
--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,71 +1,19 @@
 import React from "react";
-import ActivityList from "./ActivityList.js";
-import Activity from "./Activity.js";
-import CompletedActivityMenu from "./CompletedActivityMenu";
-import Duration from "./Duration.js";
-import useDate from "../hooks/useDate.js";
+import { Container, Box } from "@mui/material";
 
-import { useActivityRepository } from "../activities/useActivityRepository.js";
-import StartedActivityMenu from "./StartedActivityMenu.js";
-
-const Layout = () => {
-    const [activityState] = useActivityRepository();
-    const currentDate = useDate();
-
-    const handleEdit = () => {
-        console.log("handle edit");
-    };
-
-    const handleResume = () => {
-        console.log("handle resume");
-    };
-
-    const handleTrash = () => {
-        console.log("handle trash");
-    };
-
-    const handleStop = () => {
-        console.log("handle stop");
-    };
-
+const styles = {
+    layoutContainer: {},
+    startActivityContainer: {},
+    activityList: {},
+};
+const Layout = ({ renderStartActivity, renderActivityList }) => {
     return (
-        <div>
-            <ActivityList
-                activities={activityState.activities}
-                renderCompletedActivity={(activity) => (
-                    <Activity
-                        activity={activity}
-                        renderMenu={() => (
-                            <CompletedActivityMenu
-                                handleEdit={() => handleEdit(activity)}
-                                handleResume={() => handleResume(activity)}
-                                handleTrash={() => handleTrash(activity)}
-                            />
-                        )}
-                        renderDuration={() => (
-                            <Duration
-                                startDate={activity.startTimeDate}
-                                endDate={activity.endTimeDate}
-                            />
-                        )}
-                    />
-                )}
-                renderStartedActivity={(activity) => (
-                    <Activity
-                        activity={activity}
-                        renderMenu={() => (
-                            <StartedActivityMenu handleStop={handleStop} />
-                        )}
-                        renderDuration={() => (
-                            <Duration
-                                startDate={activity.startTimeDate}
-                                endDate={currentDate}
-                            />
-                        )}
-                    />
-                )}
-            />
-        </div>
+        <Container sx={styles.layoutContainer}>
+            <Box sx={styles.startActivityContainer}>
+                {renderStartActivity()}
+            </Box>
+            <Box sx={styles.activityList}>{renderActivityList()}</Box>
+        </Container>
     );
 };
 
