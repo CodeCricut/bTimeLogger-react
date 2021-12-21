741029ab9c8712bfa57c1a79a399baeea1c33404
Andrew Richerson <ajorigami1@gmail.com>
2021-12-21 14:59:01 -0600

    Display only starting time if no ending time; add sytling

--- frontend/src/components/ActivityTimes.js
+++ frontend/src/components/ActivityTimes.js
@@ -1,17 +1,45 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import Moment from "react-moment";
+
+import { ActivityModel } from "../activities/ActivityModel";
 
 const style = {
     fontSize: 12,
     color: "text.secondary",
+    dateTime: {
+        fontWeight: "bold",
+        fontFamily: "Courier",
+        fontSize: "80%",
+        verticalAlign: "center",
+    },
 };
 
+/**
+ * @param {object} props
+ * @param {ActivityModel} props.activity
+ */
 const ActivityTimes = ({ activity }) => {
     return (
         <Typography variant="subtitle1" sx={style}>
-            <Moment>{activity.startTimeDate}</Moment> -{" "}
-            <Moment>{activity.endTimeDate}</Moment>
+            {activity.endTimeDate ? (
+                <React.Fragment>
+                    <Box sx={style.dateTime} component="span">
+                        <Moment>{activity.startTimeDate}</Moment>
+                    </Box>
+                    {" - "}
+                    <Box sx={style.dateTime} component="span">
+                        <Moment>{activity.endTimeDate}</Moment>
+                    </Box>
+                </React.Fragment>
+            ) : (
+                <React.Fragment>
+                    Started at{" "}
+                    <Box sx={style.dateTime} component="span">
+                        <Moment>{activity.startTimeDate}</Moment>
+                    </Box>
+                </React.Fragment>
+            )}
         </Typography>
     );
 };
