fd9feb35c7b695a3f7b978b4f9261c2897783891
Andrew Richerson <ajorigami1@gmail.com>
2021-12-24 06:21:37 -0600

    Add jsdocs to SearchParams as per #26

--- frontend/src/model/SearchParams.js
+++ frontend/src/model/SearchParams.js
@@ -1,6 +1,8 @@
-import moment from "moment";
 import { formatQueryDate } from "../util/timeFormatters";
 
+/**
+ * Parameters for searching for activities.
+ */
 export default class SearchParams {
     searchTerm = "";
     selectedType = "";
@@ -11,6 +13,10 @@ export default class SearchParams {
         obj && Object.assign(this, obj);
     }
 
+    /**
+     * Get a query string in the format of `selectedType={selected type}&fromDate={from date}&toDate={to date}&searchTerm={search term}`.
+     * When a field is null or empty, then the query string will not contain that part of the string.
+     */
     get queryString() {
         let queryStr = "";
         if (this.selectedType) queryStr += `selectedType=${this.selectedType}&`;
@@ -24,12 +30,18 @@ export default class SearchParams {
         return queryStr;
     }
 
+    /**
+     * @returns True if either date is populated.
+     */
     get doSearchBetweenDates() {
         return !!(this.fromDate || this.toDate);
     }
 
     /**
-     * Return a new SearchParams object based on the queryStr
+     * Return a new SearchParams object based on a query string in the format of `selectedType={selected type}&fromDate={from date}&toDate={to date}&searchTerm={search term}`.
+     * If a part of the query string is missing, it will not be populated on the object.
+     * @param {string} queryStr The query string to parse.
+     * @returns {SearchParams} The built object.
      */
     static parseQueryString(queryStr) {
         const searchParams = new SearchParams();
@@ -40,7 +52,7 @@ export default class SearchParams {
             const [key, value] = part.split("=");
 
             if (key === "fromDate" || key === "toDate")
-                searchParams[key] = moment(value).toDate();
+                searchParams[key] = new Date(value);
             else if (!value) searchParams.searchTerm = key;
             else searchParams[key] = value;
         }
