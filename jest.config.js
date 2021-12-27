d51658fc317dd718e4f1650b68839f56e59d7eca
Andrew Richerson <ajorigami1@gmail.com>
2021-12-27 16:22:45 -0600

    Change coverage path to be in server dir

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
     collectCoverage: true,
+    coverageDirectory: "coverage", // server/coverage
     moduleFileExtensions: ["js", "mjs"],
     transform: {
         "^.+\\.js$": "babel-jest",
