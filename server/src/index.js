cc539797e1f07c706816e00c3d03bad89c25ebd9
Andrew Richerson <ajorigami1@gmail.com>
2021-12-26 23:17:54 -0600

    Set up routes and ports for heroku
    In order to host the app on Heroku, we need to do a few things:
    1. Use an environment variable (MONGODB_URI) for the connection string to the Mongo DB
    2. Host the frontend and backend on a single website.
    In order to do this, we run a postbuild script to build the frontend, then serve those static
    files with express.
    3. Use an environment variable (PORT) as the port if defined (defined by Heroku)

--- server/src/index.js
+++ server/src/index.js
@@ -4,23 +4,35 @@ import typesRouter from "./routers/types.js";
 import activitiesRouter from "./routers/activities.js";
 import docsRouter from "./routers/docs.js";
 import cors from "cors";
-
+import path from "path";
 import mongoose from "mongoose";
 
 dotenv.config();
+const __dirname = path.resolve();
+
+// path.join(__dirname, "build")
+const clientBuildPath = path.join(__dirname, "frontend/build");
+console.log(clientBuildPath);
 
 const connectToDatabase = async () => {
-    const dbConnectionString = "mongodb://localhost/b_time_logger";
+    // MONGODB_URI will be defined in Heroku
+    const dbConnectionString =
+        process.env.MONGODB_URI || "mongodb://localhost/b_time_logger";
     await mongoose.connect(dbConnectionString, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
 };
 
+const setupStaticReactAssets = (app) => {
+    app.use(express.static(clientBuildPath));
+};
+
 const setUpServerApp = (port) => {
     const app = express();
     app.use(cors());
     app.use(express.json());
+    setupStaticReactAssets(app);
     return app;
 };
 
@@ -28,12 +40,17 @@ const setAppRoutes = (app) => {
     app.use("/types", typesRouter);
     app.use("/activities", activitiesRouter);
     app.use("/docs", docsRouter);
+
+    // Setup React routing
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(clientBuildPath, "index.html"));
+    });
 };
 
 try {
     await connectToDatabase();
 
-    const port = process.env.SERVER_PORT;
+    const port = process.env.PORT || process.env.TEST_SERVER_PORT;
     const app = setUpServerApp(port);
     setAppRoutes(app);
 
