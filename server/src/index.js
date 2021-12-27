e26e7aa3ced3e95878f62d190f14939bd44d5c64
Andrew Richerson <ajorigami1@gmail.com>
2021-12-26 23:57:53 -0600

    Refactor

--- server/src/index.js
+++ server/src/index.js
@@ -8,50 +8,14 @@ import path from "path";
 import mongoose from "mongoose";
 
 dotenv.config();
-const __dirname = path.resolve();
-
-// path.join(__dirname, "build")
-const clientBuildPath = path.join(__dirname, "frontend/build");
-console.log(clientBuildPath);
-
-const connectToDatabase = async () => {
-    // MONGODB_URI will be defined in Heroku
-    const dbConnectionString =
-        process.env.MONGODB_URI || "mongodb://localhost/b_time_logger";
-    await mongoose.connect(dbConnectionString, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-};
-
-const setupStaticReactAssets = (app) => {
-    app.use(express.static(clientBuildPath));
-};
-
-const setUpServerApp = (port) => {
-    const app = express();
-    app.use(cors());
-    app.use(express.json());
-    setupStaticReactAssets(app);
-    return app;
-};
-
-const setAppRoutes = (app) => {
-    app.use("/types", typesRouter);
-    app.use("/activities", activitiesRouter);
-    app.use("/docs", docsRouter);
-
-    // Setup React routing
-    app.get("/*", (req, res) => {
-        res.sendFile(path.join(clientBuildPath, "index.html"));
-    });
-};
+const baseDirectory = path.resolve();
 
 try {
     await connectToDatabase();
+    const port = getPort();
 
-    const port = process.env.PORT || process.env.TEST_SERVER_PORT;
-    const app = setUpServerApp(port);
+    const app = express();
+    setupAppMiddleware(app);
     setAppRoutes(app);
 
     app.listen(port, () =>
@@ -60,4 +24,59 @@ try {
 } catch (e) {
     console.error("Connection error:" + e);
     process.exit(1);
+}
+
+function setupAppMiddleware(app) {
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.static(getClientBuildPath()));
+    return app;
+}
+
+async function connectToDatabase() {
+    await mongoose.connect(getMongoConnection(), {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+}
+
+function setAppRoutes(app) {
+    app.use("/types", typesRouter);
+    app.use("/activities", activitiesRouter);
+    app.use("/docs", docsRouter);
+
+    // Setup React routing
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(getClientBuildPath(), "index.html"));
+    });
+}
+
+function getPort() {
+    const port = process.env.PORT || process.env.TEST_SERVER_PORT;
+    if (!port) {
+        throw new Error(
+            "Tried to start app without port defined. Please define a PORT or TEST_SERVER_PORT environment variable in the .env file."
+        );
+    }
+    return port;
+}
+
+function getClientBuildPath() {
+    const clientBuildPath = path.join(baseDirectory, "frontend/build");
+    if (!clientBuildPath) {
+        throw new Error(
+            `Could not resolve client build path.\tBase directory:${baseDirectory}`
+        );
+    }
+    return clientBuildPath;
+}
+
+function getMongoConnection() {
+    const connection = process.env.MONGODB_URI;
+    if (!connection) {
+        throw new Error(
+            "Tried to start app without Mongo DB connection string defined. Please define a MONGODB_URI environment variable in the .env file."
+        );
+    }
+    return connection;
 }
